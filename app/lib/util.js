9f8a802495cdb892fde62aa7d15430b92ca859ce
Hans Lombard <hans@platsoft.net>
2014-12-01 13:47:06 +0200

    docmentation

--- app/lib/util.js
+++ app/lib/util.js
@@ -29,7 +29,20 @@ gutil.prototype.formatDateTime = function(dte) {
  * muid = unique message id, 32 bit integer
  * command = command, 32 bit integer
  * obj = message data
+ * 
+ * message structure:
+ * <0x01               0x00 0x00 0x00 0x00    0x00 0x00 0x00 0x01  0x00 0x00 0x00 0x00     .. .. .. .. ..  
+ *  START OF HEADER    UNIQUE MESSAGE ID      COMMAND INTEGER      PAYLOAD LENGTH          UTF8 encoded payload
+ *  
+ *  Fields explained:
+ *  
+ *  START OF HEADER   (ASCII control ^A) 1 byte
+ *  UNIQUE MESSAGE ID A unique ID to identify this request 32-bit unsigned integer
+ *  COMMAND INTEGER  32 bit unsigned integer identifiying the command
+ *  PAYLOAD LENGTH  32 bit unsigned integer speciifcying the payload size
+ *  PAYLOAD	    UTF-8 encoded message data
  *
+ *  Note: All integers are unsigned big endian
  */
 gutil.encodeMessage = function (muid, command, obj) {
 	var str_data = JSON.stringify(obj);
