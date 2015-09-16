9e3b05c837d1bab13bf6166a849821b1ed917f0d
Johan <johan@platsoft.net>
2015-09-16 08:46:01 +0200

    Adjusting create_api default function names

--- app/lib/app.js
+++ app/lib/app.js
@@ -293,7 +293,7 @@ exports = module.exports = function(_o) {
 					name              : param.name + '.' + op,
 					method            : 'post',
 					url               : param.url_prefix + param.name,
-					db_function       : param.db_schema + '.save' + param.name,
+					db_function       : param.db_schema + '.save_' + param.name,
 					validation_string : entry.validation_string
 				});
 			}
@@ -303,7 +303,7 @@ exports = module.exports = function(_o) {
 					name              : param.name + '.' + op,
 					method            : 'post',
 					url               : param.url_prefix + param.name + '/:' + key_val,
-					db_function       : param.db_schema + '.save' + param.name,
+					db_function       : param.db_schema + '.save_' + param.name,
 					validation_string : entry.validation_string
 				});
 			}
@@ -313,7 +313,7 @@ exports = module.exports = function(_o) {
 					name              : param.name + '.' + op,
 					method            : 'post',
 					url               : param.url_prefix + param.name + '/:' + key_val + '/' + op,
-					db_function       : param.db_schema + '.save' + param.name,
+					db_function       : param.db_schema + '.' + op + '_' + param.name,
 					validation_string : entry.validation_string
 				});
 			}
