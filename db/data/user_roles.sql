6d53fb93169974937f26f786bc65916f7537d023
Johan <johan@platsoft.net>
2015-11-24 10:57:15 +0200

    Adding user_role for system -> admin

--- db/data/user_roles.sql
+++ db/data/user_roles.sql
@@ -1,4 +1,3 @@
-
 INSERT INTO grape.access_role (role_name) VALUES ('guest');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/session/new');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/lookup/.*');
@@ -7,3 +6,4 @@ INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/a
 INSERT INTO grape.access_role (role_name) VALUES ('admin');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('admin', '.*');
 
+INSERT INTO grape.user_role (user_id, role_name) VALUES (999, 'admin');
