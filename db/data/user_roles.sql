b9325a94acdf488e1f5d2dca6e1c4eede993a176
Hans Lombard <hans@platsoft.net>
2016-05-11 12:22:17 +0200

    cleanup roles ('guest' can now only access /grape/login, and other paths moved to 'all' role)

--- db/data/user_roles.sql
+++ db/data/user_roles.sql
@@ -1,13 +1,13 @@
 INSERT INTO grape.access_role (role_name) VALUES ('guest');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/session/new');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/lookup/.*');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/list');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/api_list');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/login');
 
 INSERT INTO grape.access_role (role_name) VALUES ('all');
+INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/lookup/.*');
+INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/list');
+INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/api_list');
+
 
 INSERT INTO grape.access_role (role_name) VALUES ('admin');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('admin', '.*');
 
-INSERT INTO grape.user_role (user_id, role_name) VALUES (999, 'admin');
