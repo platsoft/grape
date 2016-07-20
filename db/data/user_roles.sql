36d2e9036be0f9de72c2e358a0beb0022cb4e43c
Hans Lombard <hans@platsoft.net>
2016-07-20 11:43:46 +0200

    allow /grape/logout and /grape/session_ping

--- db/data/user_roles.sql
+++ db/data/user_roles.sql
@@ -1,11 +1,13 @@
 INSERT INTO grape.access_role (role_name) VALUES ('guest');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/session/new');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/login');
+INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/session_ping');
 
 INSERT INTO grape.access_role (role_name) VALUES ('all');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/lookup/.*');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/list');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/api_list');
+INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/logout');
 
 
 INSERT INTO grape.access_role (role_name) VALUES ('admin');
