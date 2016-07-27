a97ca900701c2070aee269d432607960e28d199f
Hans Lombard <hans@platsoft.net>
2016-07-27 07:20:28 +0200

    patch

--- db/patch/0.9.sql
+++ db/patch/0.9.sql
@@ -14,6 +14,20 @@ ALTER TABLE grape.reports_executed ADD COLUMN input_fields JSON;
 ALTER TABLE grape.schedule ADD COLUMN progress_completed INTEGER DEFAULT 0;
 ALTER TABLE grape.schedule ADD COLUMN progress_total INTEGER DEFAULT 0;
 
+CREATE TABLE grape.session_history(
+	session_id text NOT NULL,
+	ip_address text,
+	user_id integer,
+	date_inserted timestamptz,
+	last_activity timestamptz,
+	date_logout timestamptz,
+	CONSTRAINT session_history_pk PRIMARY KEY (session_id)
+
+);
+ALTER TABLE grape.session_history ADD CONSTRAINT session_user_fk FOREIGN KEY (user_id) 
+	REFERENCES grape."user" (user_id) MATCH FULL;
+
+
 SELECT grape.set_value('grape_version', '0.9');
 
 
