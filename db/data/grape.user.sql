df0bdcb752703e9a53edc335ce51bfecb0c876b5
Hans Lombard <hans@platsoft.net>
2016-08-30 09:47:54 +0200

    fix wrong column name

--- db/data/grape.user.sql
+++ db/data/grape.user.sql
@@ -1,5 +1,5 @@
 
 
 -- SYSTEM
-INSERT INTO grape."user" (password, username, email, fullnames, active, is_local) VALUES ('plakvark','System','info@platsoft.net', 'System user', true, true);
+INSERT INTO grape."user" (password, username, email, fullnames, active, local_only) VALUES ('plakvark','System','info@platsoft.net', 'System user', true, true);
 
