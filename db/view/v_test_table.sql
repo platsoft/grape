32a5c3ed729e6c0b97d83f8c5c94e674eeab0e4f
Ettienne <ettienne@platsoft.net>
2016-10-26 11:59:46 +0200

    added test table view with username

---
+++ db/view/v_test_table.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW grape.v_test_table AS
+	SELECT 
+		test_table_id,
+		table_schema,
+		table_name,
+		description,
+		date_created,
+		user_id,
+		date_updated,
+		username(user_id) AS username
+	FROM grape.test_table;
