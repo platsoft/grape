fb96819fb90cdf65a948707c086a01b039ccb5b1
Hans Lombard <hans@platsoft.net>
2017-01-17 17:23:39 +0200

    add whitelist tables for admin roles

---
+++ db/data/whitelist_tables.sql
@@ -0,0 +1,4 @@
+
+SELECT grape.list_query_whitelist_add('grape', 
+	'{"user", "user_role", "user_access_role", "report", "list_query_whitelist", "process", "schedule", "setting", "data_import", "data_import_type"}', '{admin}'::TEXT[]);
+
