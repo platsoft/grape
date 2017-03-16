66f0f1d4eb88a43078502598510fa987faaec354
Hans Lombard <hans@platsoft.net>
2017-03-16 13:00:00 +0200

    Generic operations on tables; new API calls: GrapeInsertRecord, GrapeUpdateRecord and GrapeDeleteRecord

--- db/patch/1.0.4.sql
+++ db/patch/1.0.4.sql
@@ -1,27 +1,10 @@
 
-
-CREATE OR REPLACE FUNCTION grape.send_email (_to TEXT, _template TEXT, _template_data JSON, _headers JSON) RETURNS INTEGER AS $$
-DECLARE
-BEGIN
-	PERFORM pg_notify('grape_send_email', 
-		json_build_object(
-			'email_template', _template, 
-			'email', _to, 
-			'template_data', _template_data,
-			'headers', _headers
-	)::TEXT);
-
-	RETURN 0;
-END; $$ LANGUAGE plpgsql;
-
-
-CREATE OR REPLACE FUNCTION grape.send_email (_to TEXT, _template TEXT, _template_data JSON) RETURNS INTEGER AS $$
-DECLARE
-BEGIN
-	PERFORM grape.send_email (_to, _template, _template_data, '{}'::JSON);
-
-	RETURN 0;
-END; $$ LANGUAGE plpgsql;
-
+CREATE TABLE grape.table_operation_whitelist(
+	schema text NOT NULL,
+	tablename text NOT NULL,
+	allowed_operation text NOT NULL,
+	roles text[],
+	CONSTRAINT insert_query_pk PRIMARY KEY (schema,tablename,allowed_operation)
+);
 
 
