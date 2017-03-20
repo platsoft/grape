b26b333d9f04149b4433f9db6a425015b9ed2b72
Hans Lombard <hans@platsoft.net>
2017-03-20 09:58:28 +0200

    standardize forgot password functionality

--- db/data/user_roles.sql
+++ db/data/user_roles.sql
@@ -2,6 +2,7 @@ INSERT INTO grape.access_role (role_name) VALUES ('guest');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/session/new');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/login');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/session_ping');
+INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/forgot_password');
 
 INSERT INTO grape.access_role (role_name) VALUES ('all');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/lookup/.*');
