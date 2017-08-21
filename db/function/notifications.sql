ba96757128a4c9c6fdd464d4927caefb9c003ffa
Hans Lombard <hans@platsoft.net>
2017-08-21 09:51:31 +0200

    notifications documentation

--- db/function/notifications.sql
+++ db/function/notifications.sql
@@ -8,8 +8,13 @@ DECLARE
 BEGIN
 	_ret := '[]'::JSONB;
 
-	FOR _r IN EXECUTE FORMAT('SELECT %I.%I()', gn.function_schema, gn.function_name) FROM grape.notification_function gn WHERE active=TRUE LOOP
-		_ret := _ret || _r;
+	FOR _function_schema, _function_name IN SELECT gn.function_schema, gn.function_name FROM grape.notification_function gn WHERE active=TRUE LOOP
+		BEGIN
+			EXECUTE FORMAT('SELECT %I.%I()', _function_schema, _function_name) INTO _r;
+			_ret := _ret || _r;
+		EXCEPTION WHEN OTHERS THEN
+			RAISE NOTICE 'Notification function error (%.%)', _function_schema, _function_name;
+		END;
 	END LOOP;
 
 	RETURN _ret;
