da053b79d3936a37cb230ba5efc841396d23f0e6
Hans Lombard <hans@platsoft.net>
2018-01-19 13:29:11 +0200

    add username to access path check result

--- db/function/session_access_path.sql
+++ db/function/session_access_path.sql
@@ -4,7 +4,8 @@ CREATE TYPE grape.access_result_type AS
 (
 	result_code INTEGER,
 	user_id INTEGER,
-	session_id TEXT
+	session_id TEXT,
+	username TEXT
 );
 
 /**
@@ -25,43 +26,45 @@ CREATE OR REPLACE FUNCTION grape.check_session_access (_session_id TEXT, _check_
 DECLARE
 	_path_role TEXT[];
 	_user_id INTEGER;
+	_username TEXT;
 BEGIN
 	SELECT user_id INTO _user_id FROM grape."session" WHERE session_id=_session_id::TEXT;
+	SELECT username INTO _username FROM grape."user" WHERE user_id=_user_id::INTEGER;
 
 	-- a list of allowed role names for this path
 	SELECT array_agg(role_name) INTO _path_role FROM grape.access_path WHERE (_check_path ~ regex_path)=TRUE;
 	IF NOT FOUND THEN
 		-- path not found. we should allow it if the grape setting default_access_allowed is true
 		IF grape.get_value('default_access_allowed', 'true') = 'true' THEN
-			RETURN (0, _user_id, _session_id)::grape.access_result_type;
+			RETURN (0, _user_id, _session_id, _username)::grape.access_result_type;
 		ELSE
-			RETURN (9, _user_id, _session_id)::grape.access_result_type;
+			RETURN (9, _user_id, _session_id, _username)::grape.access_result_type;
 		END IF;
 	END IF;
 
 	-- everyone (even when not logged in) has access to "guest" role
 	IF 'guest' = ANY (_path_role) THEN
-		RETURN (0, _user_id, _session_id)::grape.access_result_type;
+		RETURN (0, _user_id, _session_id, _username)::grape.access_result_type;
 	END IF;
 
 	-- Invalid session
 	IF _user_id IS NULL THEN
-		RETURN (1, _user_id, _session_id)::grape.access_result_type;
+		RETURN (1, _user_id, _session_id, _username)::grape.access_result_type;
 	END IF;
 
 	-- everyone that is logged in has access to the "all" role
 	IF 'all' = ANY (_path_role) THEN
-		RETURN (0, _user_id, _session_id)::grape.access_result_type;
+		RETURN (0, _user_id, _session_id, _username)::grape.access_result_type;
 	END IF;
 
 
 	-- user_role exists for this user and one of the path's roles
 	IF EXISTS (SELECT 1 FROM grape.user_role WHERE user_id=_user_id::INTEGER AND role_name = ANY (_path_role)) THEN
-		RETURN (0, _user_id, _session_id)::grape.access_result_type;
+		RETURN (0, _user_id, _session_id, _username)::grape.access_result_type;
 	END IF;
 
 	-- the user is not added to any of the roles for this path, Permission denied
-	RETURN (2, _user_id, _session_id)::grape.access_result_type;
+	RETURN (2, _user_id, _session_id, _username)::grape.access_result_type;
 END; $$ LANGUAGE plpgsql;
 
 
