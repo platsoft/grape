e2272cea31e2843717d7d6accbc7b49d9e3d505c
Hans Lombard <hans@platsoft.net>
2018-01-29 20:03:28 +0200

    add support for multiple files in upload; lowercase header key

--- app/api/data_import.js
+++ app/api/data_import.js
@@ -3,7 +3,9 @@ var app;
 var fs = require('fs');
 var async = require('async');
 var XLSX = require('xlsx');
+var _ = require('underscore');
 var csvparse = require('csv-parse');
+var ds = null;
 
 exports = module.exports = function(_app) {
 	app = _app;
@@ -146,8 +148,82 @@ function api_data_import_process(req, res)
 	res.locals.db.json_call('grape.data_import_process', req.body, null, {response: res});
 }
 
+function process_data_import_file(file, data_import_id, item_queue)
+{
+	var originalname = file.originalFilename; 
+	var filename = originalname.replace(/.*(\.[^.]*$)/, 'data_import_' + data_import_id + '$1')
+	ds.saveFile('dimport', file.path, filename, false);
+
+	if (originalname.search('.xls') != -1 || originalname.search('.xlsx') != -1 )
+	{
+		var workbook = XLSX.readFile(file.path);
+		var first_sheet_name = workbook.SheetNames[0];
+		var worksheet = workbook.Sheets[first_sheet_name];
+		
+		var range = worksheet['!ref'];
+		var ar = range.split(':');
+
+		var d = XLSX.utils.decode_range(range);
+
+		var max_col = d.e.c;
+		var max_row = d.e.r;
+
+		var headers = [];
+		for (var col = 0; col <= max_col; col++)
+		{
+			var address = XLSX.utils.encode_cell({r: 0, c: col});
+			if (worksheet[address])
+				headers.push(worksheet[address].v.replace(/[^A-Z]/gi, '').toLowerCase());
+			else
+				headers.push(address.toLowerCase());
+		}
+
+		for (var row = 1; row <= max_row; row++)
+		{
+			var data = {data:{}};
+			data['data_import_id'] = data_import_id;
+			for (var col = 0; col <= max_col; col++)
+			{
+				var val = '';
+				var address = XLSX.utils.encode_cell({r: row, c: col});
+				if (worksheet[address])
+					val = worksheet[address].v;
+				
+				data.data[headers[col]] = val;
+			}
+			
+			item_queue.push(data);
+
+		}
+		
+		if (row == 1)
+			item_queue.drain();
+	}
+	else if (originalname.search('.csv') != -1 ) 
+	{
+		var rs = fs.createReadStream(file.path);
+		
+		var parser = csvparse({columns: true}, function(err, data) {
+			//TODO error check
+			for (var i = 0; i < data.length; i++)
+			{
+				var row = {data:{}}
+				row['data_import_id'] = data_import_id;
+				row.data = data[i];
+				
+				item_queue.push(row);
+			}
+		});
+
+		rs.pipe(parser);
+	}
+
+}
+
 function api_data_import(req, res)
 {
+	ds = app.get('document_store');
+
 	var item_count = 0;
 	var errors = [];
 	var data_import_ids = [];
@@ -177,91 +253,40 @@ function api_data_import(req, res)
 	if (filefields.length == 0)
 	{
 		res.status(200).json({'status': 'ERROR', 'message': 'No files', 'code': -1});
+		return;
 	}
+
+	var description = req.body.description;
+	var processing_function = req.body.processing_function;
+	var processing_param = {};
+
+	try {
+		processing_param = JSON.parse(req.body.processing_param);
+	} catch (e) { processing_param = {}; }
+
 
 	for (var i = 0; i < filefields.length; i++)
 	{
-		var file = req.files[filefields[i]];
-		var ds = app.get('document_store');
-		var description = req.body.description;
-		var processing_function = req.body.processing_function;
-		var processing_param = {};
-		console.log(req.body);
+		var file_field = req.files[filefields[i]];
+		if (!_.isArray(file_field))
+			var files = [file_field];
+		else
+			var files = file_field;
 
-		try {
-			processing_param = JSON.parse(req.body.processing_param);
-		} catch (e) { processing_param = {}; }
+		files.forEach(function(file) {
 
-
-		res.locals.db.json_call('grape.data_import_insert', {processing_function: processing_function, filename: file.originalFilename, description: description, processing_param: processing_param}, function(err, result) { 
-			
-			var data_import_id = result.rows[0].grapedata_import_insert.data_import_id;
-			var originalname = file.originalFilename; 
-			var filename = originalname.replace(/.*(\.[^.]*$)/, 'data_import_'+data_import_id+'$1')
-			ds.saveFile('dimport', file.path, filename, false);
-			data_import_ids.push(data_import_id);
-			if (originalname.search('.xls') != -1 || originalname.search('.xlsx') != -1 ) {
-				var workbook = XLSX.readFile(file.path);
-				var first_sheet_name = workbook.SheetNames[0];
-				var worksheet = workbook.Sheets[first_sheet_name];
+			res.locals.db.json_call('grape.data_import_insert', 
+			{	processing_function: processing_function, 
+				filename: file.originalFilename, 
+				description: description, 
+				processing_param: processing_param
+			}, function(err, result) { 
 				
-				var range = worksheet['!ref'];
-				var ar = range.split(':');
+				var data_import_id = result.rows[0].grapedata_import_insert.data_import_id;
+				data_import_ids.push(data_import_id);
 
-				var d = XLSX.utils.decode_range(range);
-
-				var max_col = d.e.c;
-				var max_row = d.e.r;
-
-				var headers = [];
-				for (var col = 0; col <= max_col; col++)
-				{
-					var address = XLSX.utils.encode_cell({r: 0, c: col});
-					if (worksheet[address])
-						headers.push(worksheet[address].v);
-					else
-						headers.push(address);
-				}
-
-				for (var row = 1; row <= max_row; row++)
-				{
-					var data = {data:{}};
-					data['data_import_id'] = data_import_id;
-					for (var col = 0; col <= max_col; col++)
-					{
-						var val = '';
-						var address = XLSX.utils.encode_cell({r: row, c: col});
-						if (worksheet[address])
-							val = worksheet[address].v;
-						
-						data.data[headers[col]] = val;
-					}
-					
-					item_queue.push(data);
-
-				}
-				
-				if (row == 1)
-					item_queue.drain();
-			}
-			else if (originalname.search('.csv') != -1 ) 
-			{
-				var rs = fs.createReadStream(file.path);
-				
-				var parser = csvparse({columns: true}, function(err, data) {
-					//TODO error check
-					for (var i = 0; i < data.length; i++)
-					{
-						var row = {data:{}}
-						row['data_import_id'] = data_import_id;
-						row.data = data[i];
-						
-						item_queue.push(row);
-					}
-				});
-
-				rs.pipe(parser);
-			}
+				process_data_import_file(file, data_import_id, item_queue);
+			});
 		});
 	}
 }
