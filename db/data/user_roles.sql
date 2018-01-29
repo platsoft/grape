5418ba1041fc3006c8d2960ecdf9c42ca8a1edbf
Hans Lombard <hans@platsoft.net>
2018-01-29 21:24:17 +0200

    add /download_public_js_files to access path

--- db/data/user_roles.sql
+++ db/data/user_roles.sql
@@ -4,12 +4,14 @@ INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/l
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/session_ping');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/forgot_password');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/get_setting_value');
+INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/download_public_js_files');
 
 INSERT INTO grape.access_role (role_name) VALUES ('all');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/lookup/.*');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/list');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/api_list');
 INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/logout');
+INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/download_public_js_files');
 
 
 INSERT INTO grape.access_role (role_name) VALUES ('admin');
