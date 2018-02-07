252bbd3bbc4c6048cd5aae8dd58bacc59c5b4f7f
Roxette <roxette@platsoft.net>
2018-02-07 09:32:26 +0200

    Minor update

--- db/data/user_roles.sql
+++ db/data/user_roles.sql
@@ -1,22 +1,25 @@
-INSERT INTO grape.access_role (role_name) VALUES ('guest');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/session/new');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/login');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/session_ping');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/forgot_password');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/grape/get_setting_value');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('guest', '/download_public_js_files');
-
-INSERT INTO grape.access_role (role_name) VALUES ('all');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/lookup/.*');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/list');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/api_list');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/grape/logout');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('all', '/download_public_js_files');
+INSERT INTO grape.access_role (role_name) 
+VALUES 
+	('guest'),
+	('all'),
+	('admin'),
+	('pg_stat'), -- role that can view pg stats
+	('switch_user') -- role that can switch to another user
+ON CONFLICT (role_name) DO NOTHING;
 
 
-INSERT INTO grape.access_role (role_name) VALUES ('admin');
-INSERT INTO grape.access_path (role_name, regex_path) VALUES ('admin', '.*');
-
-INSERT INTO grape.access_role (role_name) VALUES ('pg_stat'); -- role that can view pg stats
-
-INSERT INTO grape.access_role (role_name) VALUES ('switch_user'); -- role that can switch to another user
+INSERT INTO grape.access_path (role_name, regex_path) 
+VALUES 
+	('guest', '/session/new'),
+	('guest', '/grape/login'),
+	('guest', '/grape/session_ping'),
+	('guest', '/grape/forgot_password'),
+	('guest', '/grape/get_setting_value'),
+	('guest', '/download_public_js_files'),
+	('all', '/lookup/.*'),
+	('all', '/grape/list'),
+	('all', '/grape/api_list'),
+	('all', '/grape/logout'),
+	('all', '/download_public_js_files'),
+	('admin', '.*')
+ON CONFLICT (role_name, regex_path, method) DO NOTHING;
