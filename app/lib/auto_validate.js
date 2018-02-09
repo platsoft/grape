5b56f3bb42569bb925f9e924364806d6db72b3d8
Hans Lombard <hans@platsoft.net>
2018-02-09 13:15:21 +0200

    added support for nested objects and arrays in validation strings; updated docs; validation tests

--- app/lib/auto_validate.js
+++ app/lib/auto_validate.js
@@ -1,10 +1,13 @@
 /**
- * Validate string syntax:
- * 	begin_expr ::= "(" <begin_expr> "," <param-def> ")"
- *	param-def ::= <param-name> ":" <data-type> <modifier-list>
- *	modifier-list ::= <opt-modifier> <modifier-list>
- *	opt-modifier ::= "*" | "E" | "0" | ""
+ * Validate string syntax (BNF):
+ * 	obj-def ::= "(" <param-def-list> ")"
+ * 	param-def-list ::= <param-def> | <param-def-list> "," <param-def>
+ *	param-def ::= <param-name> ":" <type-info> <modifier-list>
+ *	type-info ::= <array-def> | <data-type> | <obj-def>
+ *	array-def ::= "[" <data-type> "]" | "[" <obj-def> "]"
  *	data-type ::= "s" | "i" | "f" | "b" | "d" | "t" | "a"
+ *	modifier-list ::= <modifier> <modifier-list>
+ *	modifier ::= "*" | "E" | "0" | ""
  *
  * Data types:
  * 	s text
@@ -12,7 +15,7 @@
  * 	f float
  * 	b boolean
  * 	d date
- * 	t date
+ * 	t datetime
  *
  * Modifiers:
  *	* optional is true (default = false)
@@ -20,58 +23,40 @@
  *	0 Nullable
  *
  * Examples:
- *	(batch_labreport_id: i, product_id: i, labreport)
+ *	(batch_labreport_id: i, product_id: i, labreport: [(product_id: i)]* )
  */
 function decode_validation_string (validate_string)
 {
-	var ret = [];
+	//console.log("Decoding string {" + validate_string + "}");
+
+	var fields = [];
 	var i = 0;
 	var state = '';
 	var var_name = '';
 	var var_info = '';
+	var datatype_indicators = ['s', 'i', 'f', 'b', 'd', 't', 'a', 'j'];
+	var current_object;
+
+	function new_empty_object()
+	{
+		var obj = new Object({
+			name: '',
+			data_type: null,
+			nullable: false,
+			optional: false,
+			empty_becomes_null: false,
+			is_object: false,
+			is_array: false,
+			items: null  // object like this one (if this is an array), or an array with objects like this (if its an object)
+		});
+		return obj;
+	}
 
 	function save ()
 	{
-		if (typeof var_info == 'object')
-		{
-			ret.push({'name': var_name, 'data_type': 'o', 'fields': var_info});
-		}
-		else // is a string
-		{
-			var modifiers = {'name': '',
-				'data_type': '',
-				'original_value': '',
-				'value': '',
-				'valid': false,
-				'nullable': false,
-				'optional': false,
-				'empty_becomes_null': false
-			};
-
-			for (var i = 0; i < var_info.length; i++)
-			{
-				switch (var_info[i])
-				{
-					case 's': case 'i': case 'f': case 'b': case 'd': case 't': case 'a': case 'j' :
-						modifiers.data_type = var_info[i];
-						continue;
-					case '*':
-						modifiers.optional = true;
-						continue;
-					case '0':
-						modifiers.nullable = true;
-						continue;
-					case 'E':
-						modifiers.empty_becomes_null = true;
-						continue;
-					default:
-						continue;
-				}
-			}
-
-			modifiers.name = var_name;
-			ret.push(modifiers);
-		}
+		fields.push(current_object);
+		current_object = new_empty_object();
+		return current_object;
 	}
 
 	for (i = 0; i < validate_string.length; i++)
@@ -85,18 +70,30 @@ function decode_validation_string (validate_string)
 					//expecting a variable next
 					state = 'var_name';
 					var_name = '';
+					current_object = new_empty_object();
 				}
 				else if (state == 'var_info') //nested object'
 				{
-					var_info = decode_validation_string (validate_string.substring(i));
-					while (validate_string[i] != ')')
-						i++;
+					var dec_result = decode_validation_string (validate_string.substring(i));
+					i += dec_result.pos;
+					current_object.items = dec_result.fields;
+					current_object.is_object = true;
+					current_object.data_type = 'object';
+				}
+				else if (state == 'array_def') // array with objects
+				{
+					var dec_result = decode_validation_string (validate_string.substring(i));
+					i += dec_result.pos;
+					current_object.items.is_object = true;
+					current_object.items.data_type = 'object';
+					current_object.items.items = dec_result.fields;
 				}
 				continue;
 			case ':':
 				//done with var_name, expecting variable info next
 				state = 'var_info';
 				var_info = '';
+				current_object.name = var_name;
 				continue;
 			case ',':
 				save();
@@ -106,18 +103,54 @@ function decode_validation_string (validate_string)
 				continue;
 			case ')':
 				save();
-				return ret;
+				//console.log("Decoding ", validate_string, " Result: ", JSON.stringify(fields, null, '   '));
+				return {fields: fields, pos: i};
+			case '[':
+				state = 'array_def';
+				current_object.is_array = true;
+				current_object.data_type = 'array';
+				current_object.items = new_empty_object();
+				continue;
+			case ']':
+				state = 'var_info';
+				continue;
+			case '*':
+				current_object.optional = true;
+				continue;
+			case '0':
+				current_object.nullable = true;
+				continue;
+			case 'E':
+				current_object.empty_becomes_null = true;
+				continue;
 
 			default:
 				switch (state)
 				{
+					case 'array_def':
+						if (datatype_indicators.indexOf(c) >= 0)
+						{
+							current_object.items.data_type = c;
+						}
+						else
+						{
+							return {error: 'Syntax error at position ' + i + ': Expected a data type indicator inside an array'};
+						}
+						continue;
 					case 'var_name':
 						if (c == ' ' || c == '\n' || c == '\t' || c == '\r')
 							continue;
 						var_name = var_name + c;
 						continue;
 					case 'var_info':
-						var_info = var_info + c;
+						if (datatype_indicators.indexOf(c) >= 0)
+						{
+							if (current_object.data_type)
+							{
+								console.log("WARNING: DATA TYPE IS ALREADY SET FOR VARIABLE " + var_name);
+							}
+							current_object.data_type = c;
+						}
 						continue;
 					default:
 						console.log("SYNTAX ERROR AT POSITION ", i);
@@ -127,189 +160,297 @@ function decode_validation_string (validate_string)
 		}
 	}
 
-	console.log("SYNTAX ERROR: NO ) FOUND TO END WITH");
+	return {error: 'Syntax error: Unexpected end of input. No matching ) found'};
+}
+
+function validate_field(field, str_value)
+{
+	if (field.data_type == 'j') //json
+	{
+		try
+		{
+			field.value = JSON.parse(str_value);
+			field.valid = true;
+		}
+		catch (e) {
+			field.valid = false;
+			p['error'] = field.name + ' must be valid json';
+		}
+	}
+	else if (field.data_type == 'i') // integer
+	{
+		if (str_value.match (/^[0-9]+$/) == null)
+		{
+			field.valid = false;
+			field['error'] = field.name + ' must be a valid integer';
+		}
+		else
+		{
+			field.valid = true;
+			field.value = parseInt(str_value);
+		}
+	}
+	else if (field.data_type == 's') // string
+	{
+		field.valid = true;
+		field.value = field.original_value;
+	}
+	else if (field.data_type == 'b') // boolean
+	{
+		if (str_value.match (/true|false|t|f/i) == null)
+		{
+			field.valid = false;
+			field['error'] = field.name + ' must be a valid boolean (false, f, true or t)';
+		}
+		else
+		{
+			field.valid = true;
+			if (str_value[0] == 'f' || str_value[0] == 'F')
+				field.value = false;
+			else
+				field.value = true;
+
+		}
+	}
+	else if (field.data_type == 'f') // float
+	{
+		if (str_value.match (/^[0-9]*(\.[0-9]*)?$/) == null)
+		{
+			field.valid = false;
+			field['error'] = field.name + ' must be a valid numeric value';
+		}
+		else
+		{
+			field.valid = true;
+			field.value = parseFloat(str_value);
+		}
+	}
+	else if (field.data_type == 'd' || field.data_type == 't') //date time
+	{
+		if (field.data_type == 'd')
+		{
+			// Non timezone-safe hack to avoid - vs / date parsing issues
+			if (!str_value.endsWith('Z'))
+				{ str_value = str_value + 'Z'; }
+		}
+
+		if (str_value.match (/^[0-9]{1,2}(\/|-)[0-9]{1,2}(\/|-)[0-9]{4}/) != null)
+		{
+			field.valid = true;
+			field.value = new Date(str_value);
+		}
+		else if (str_value.match (/^[0-9]{4}(\/|-)[0-9]{1,2}(\/|-)[0-9]{1,2}/) != null)
+		{
+			field.valid = true;
+			field.value = new Date(str_value);
+		}
+		else
+		{
+			field.valid = false;
+		}
+	}
+	else if (field.data_type == 'a')
+	{
+		field.value = field.original_value;
+		if (typeof field.original_value == 'object')
+		{
+			field.valid = true;
+		}
+		else
+		{
+			field.valid = false;
+		}
+	}
+	else
+	{
+		field.valid = true;
+		field.value = field.original_value;
+	}
+
+}
+
+
+//params is an array with validation parameters (as returned from decode_validation_string)
+//obj is the object we are validating
+function validate_object (obj, params)
+{
+	var f_params = params;
+	var errors = [];
+
+	for (var i = 0; i < f_params.length; i++)
+	{
+		var p = f_params[i];
+		p.original_value = null;
+		p.value = null;
+		p.valid = false;
+		p.errors = [];
+
+		var value_in_object = obj[p.name];
+		
+		p.original_value = value_in_object;
+
+		if (typeof value_in_object == 'undefined')
+		{
+			if (p.optional == false)
+			{
+				p.errors.push(p.name + ' is a required field');
+				errors.push('Required field "' + p.name + '" is missing');
+				continue;
+			}
+			else
+			{
+				p.valid = true;
+				p.value = undefined;
+				continue;
+			}
+		}
+
+
+		if (p.is_object)
+		{
+			if (typeof value_in_object != 'object')
+			{
+				p.valid = false;
+				p.errors.push(p.name + ' is supposed to be an object');
+				errors.push('The field "' + p.name + '" is supposed to be an object');
+			}
+
+			var validate_ret = validate_object(value_in_object, p.items);
+			if (validate_ret.errors.length > 0)
+			{
+				validate_ret.errors.forEach(function(e) {
+					errors.push(['Error in "', p.name, '": ', e].join(''));
+					p.errors.push(e);
+				});
+				p.value = null;
+			}
+			else
+			{
+				p.value = p.original_value;
+			}
+			p.items.items = validate_ret.fields;
+		}
+		else if (p.is_array)
+		{
+			if (typeof value_in_object == 'object' && value_in_object.length) // make sure it's an array
+			{
+				var found_error = false;
+				var new_items = [];
+
+				if (p.items.is_object) // array of objects
+				{
+					for (var j = 0; j < value_in_object.length; j++)
+					{
+						if (typeof value_in_object[j] == 'object')
+						{
+							var validate_result = validate_object(value_in_object[j], p.items.items);
+							if (validate_result.errors.length == 0)
+							{
+								new_items.push(validate_result.fields);
+							}
+							else
+							{
+								p.valid = false;
+								validate_result.errors.forEach(function(e) {
+									var err = 'Error in array item #' + j + ' in "' + p.name + '": ' + e;
+									errors.push(err);
+									p.errors.push(err);
+								});
+							}
+						}
+						else
+						{
+							p.valid = false;
+							var err = 'Array item #' + j + ' in "' + p.name + '" is supposed to be an object';
+							p.errors.push(err);
+							errors.push(err);
+						}
+					}
+				
+					p.valid = true;
+				}
+				else // array of simple values
+				{
+					for (var j = 0; j < value_in_object.length; j++)
+					{
+						var field = new Object(p.items);
+						field.original_value = value_in_object[j];
+						validate_field(field, value_in_object[j].toString());
+						if (field.valid)
+						{
+							new_items.push(field.value);
+						}
+						else
+						{
+							p.valid = false;
+							p['error'] = 'Error in "' + p.name + '" array item #' + j + ': ' + field.error;
+							errors.push(p['error']);
+							
+							break;
+						}
+					}
+					p.value = new_items;
+				}
+			}
+			else
+			{
+				p.valid = false;
+				var err = p.name + ' is supposed to be an array';
+				p.errors.push(err);
+				errors.push('The field ' + p.name + ' is supposed to be an array');
+			}
+		}
+		else
+		{
+
+
+			if (value_in_object !== null)
+			{
+				var str_value = value_in_object.toString();
+				if (str_value == '' && p.empty_becomes_null == true)
+				{
+					value_in_object = null;
+				}
+			}
+
+			if (value_in_object == null)
+			{
+				if (p.nullable == false)
+				{
+					p['error'] = p.name + ' cannot be null';
+					errors.push(p['error']);
+					p.valid = false;
+					continue;
+				}
+				else
+				{
+					p.valid = true;
+					p.value = null;
+					continue;
+				}
+			}
+
+			validate_field(p, str_value);
+
+			obj[p.name] = p.value;
+			if (p['error'])
+				errors.push(p['error']);
+
+		}
+	}
+
+	return {fields: f_params, errors: errors};
 }
 
 
 function auto_validate(obj, validate_string)
 {
-	var params = decode_validation_string (validate_string);
+	var dec_result = decode_validation_string (validate_string);
+	if (dec_result.error)
+		return {obj: null, errors: [dec_result.error]};
+	var validate_res = validate_object(obj, dec_result.fields);
 
-	var validation_errors = [];
-
-	function fill_validation_object (obj, params)
-	{
-		var f_params = params;
-
-		for (var i = 0; i < f_params.length; i++)
-		{
-			var p = f_params[i];
-			var value_in_object = obj[p.name];
-			if (p.data_type == 'o')
-			{
-				p.fields = fill_validation_object (value_in_object, p.fields);
-			}
-			else
-			{
-				p.original_value = value_in_object;
-
-				if (typeof value_in_object == 'undefined')
-				{
-					if (p.optional == false)
-					{
-						p['error'] = p.name + ' is a required field';
-						validation_errors.push(p['error']);
-						continue;
-					}
-					else
-					{
-						p.valid = true;
-						p.value = undefined;
-						continue;
-					}
-				}
-
-				if (value_in_object !== null)
-				{
-					var str_value = value_in_object.toString();
-					if (str_value == '' && p.empty_becomes_null == true)
-					{
-						value_in_object = null;
-					}
-				}
-
-				if (value_in_object == null)
-				{
-					if (p.nullable == false)
-					{
-						p['error'] = p.name + ' cannot be null';
-						validation_errors.push(p['error']);
-						p.valid = false;
-						continue;
-					}
-					else
-					{
-						p.valid = true;
-						p.value = null;
-						continue;
-					}
-				}
-
-				if (p.data_type == 'j')
-				{
-					try
-					{
-						p.value = JSON.parse(str_value);
-						p.valid = true;
-					}
-					catch (e) {
-						p.valid = false;
-						p['error'] = p.name + ' must be valid json';
-					}
-				}
-				else if (p.data_type == 'i')
-				{
-					if (str_value.match (/^[0-9]+$/) == null)
-					{
-						p.valid = false;
-						p['error'] = p.name + ' must be a valid integer';
-					}
-					else
-					{
-						p.valid = true;
-						p.value = parseInt(str_value);
-					}
-				}
-				else if (p.data_type == 's')
-				{
-					p.valid = true;
-					p.value = p.original_value;
-				}
-				else if (p.data_type == 'b')
-				{
-					if (str_value.match (/true|false|t|f/i) == null)
-					{
-						p.valid = false;
-						p['error'] = p.name + ' must be a valid boolean (false, f, true or t)';
-					}
-					else
-					{
-						p.valid = true;
-						if (str_value[0] == 'f' || str_value[0] == 'F')
-							p.value = false;
-						else
-							p.value = true;
-
-					}
-				}
-				else if (p.data_type == 'f')
-				{
-					if (str_value.match (/^[0-9]*(\.[0-9]*)?$/) == null)
-					{
-						p.valid = false;
-						p['error'] = p.name + ' must be a valid numeric value';
-					}
-					else
-					{
-						p.valid = true;
-						p.value = parseFloat(str_value);
-					}
-				}
-				else if (p.data_type == 'd' || p.data_type == 't')
-				{
-					if (p.data_type == 'd')
-					{
-						// Non timezone-safe hack to avoid - vs / date parsing issues
-						if (!str_value.endsWith('Z'))
-							{ str_value = str_value + 'Z'; }
-					}
-
-					if (str_value.match (/^[0-9]{1,2}(\/|-)[0-9]{1,2}(\/|-)[0-9]{4}/) != null)
-					{
-						p.valid = true;
-						p.value = new Date(str_value);
-					}
-					else if (str_value.match (/^[0-9]{4}(\/|-)[0-9]{1,2}(\/|-)[0-9]{1,2}/) != null)
-					{
-						p.valid = true;
-						p.value = new Date(str_value);
-					}
-					else
-					{
-						p.valid = false;
-					}
-				}
-				else if (p.data_type == 'a')
-				{
-					p.value = p.original_value;
-					if (typeof p.original_value == 'object')
-					{
-						p.valid = true;
-					}
-					else
-					{
-						p.valid = false;
-					}
-				}
-				else
-				{
-					p.valid = true;
-					p.value = p.original_value;
-				}
-
-				obj[p.name] = p.value;
-				if (p['error'])
-					validation_errors.push(p['error']);
-
-			}
-		}
-
-		return f_params;
-	}
-
-	var ret = fill_validation_object (obj, params);
-
-	return {obj: ret, errors: validation_errors};
+	return {obj: validate_res.fields, errors: validate_res.errors};
 }
 
 module.exports.validate = auto_validate;
---
+++ test/validate.js
@@ -0,0 +1,228 @@
+
+
+var colors = require('colors');
+
+var level = 0;
+
+function print_info (str) { console.error("  ".repeat(level) + colors.blue(str)); }
+function print_ok (str) { console.error("  ".repeat(level) + colors.green(str)); }
+function print_warn (str) { console.error("  ".repeat(level) + colors.yellow(str)); }
+function print_err (str) { console.error("  ".repeat(level) + colors.red(str)); }
+
+
+var auto_validate = require('../app/lib/auto_validate.js');
+
+function test(descr, obj, string, check_func)
+{
+	console.log(descr);
+
+	var ret = auto_validate.validate(obj, string);
+
+	//console.log(JSON.stringify(ret, null, '    '));
+	
+	if (check_func)
+	{
+		var result = check_func(ret);
+		if (ret.errors.length > 0)
+			print_warn(ret.errors.join("\n"));
+		if (result)
+			print_ok('OK');
+		else
+			print_err('FAILED');
+	}
+
+	console.log();
+}
+
+/*
+var ret = auto_validate.validate({product_id: 1}, '(product_id:i)');
+console.log(ret);
+
+ret = auto_validate.validate({product_id: 1, message: 'abc'}, '(product_id:i,message:s)');
+console.log(ret);
+ret = auto_validate.validate({product_id: 1, message: 'abc', list: [2,3,4]}, '(product_id:i,message:s,list:[i])');
+console.log(ret);
+*/
+/*
+ret = auto_validate.validate({product_id: 1, message: 'abc', list: [2,3,'s']}, '(product_id:i,message:s,list:[i])');
+console.log(ret);
+*/
+/*
+ret = auto_validate.validate({product: {product_id:1,item:{name:'string_value'}}}, '(product:(product_id:i,item:(name:s)))');
+console.log(JSON.stringify(ret, null, '    '));
+*/
+
+function check_errors_0(obj)
+{
+	if (obj.errors.length)
+		return false;
+	else
+		return true;
+}
+function check_errors_1(obj)
+{
+	if (obj.errors.length == 1)
+		return true;
+	else
+		return false;
+}
+function check_errors_2(obj)
+{
+	if (obj.errors.length == 2)
+		return true;
+	else
+		return false;
+}
+
+
+
+test('Testing valid integers', {
+	product_id: 10109
+}, "(product_id:i)", check_errors_0);
+test('Testing invalid integers', {
+	product_id: 10109.23
+}, "(product_id:i)", check_errors_1);
+test('Testing invalid integers', {
+	product_id: 'sdsds'
+}, "(product_id:i)", check_errors_1);
+
+
+
+
+test('Testing valid boolean', {
+	stuff: true
+}, "(stuff:b)", check_errors_0);
+
+test('Testing invalid boolean', {
+	stuff: 3
+}, "(stuff:b)", check_errors_1);
+
+
+
+
+test('Testing optional modifier', {
+	product_id: 1
+}, "(product_id:i,optional_id:i*)", check_errors_0);
+
+
+test('Testing nullable modifier', {
+	product_id: 1,
+	optional_id: null
+}, "(product_id:i,optional_id:i0)", check_errors_0);
+test('Testing without modifier', {
+	product_id: 1,
+	optional_id: null
+}, "(product_id:i,optional_id:i)", check_errors_1);
+
+
+
+
+
+
+test('Testing nested objects', {
+	product: {
+		def: {
+			product_id: 1
+		},
+		message: 'abc',
+		def2: {
+			p1: 9,
+			p2: 'abc',
+			def3: {
+				abc: 'abc'
+			}
+			
+		}
+	}
+}, "(product: (def: (product_id:i), def2: (p1:i,p2:s,def3:(abc:s)), message:s))", check_errors_0);
+
+
+test('Testing arrays', {
+	product_ids: [1,3,4,5]
+}, "(product_ids:[i])", check_errors_0);
+
+
+test('Testing arrays inside object', {
+	stuff: {product_ids: [1,3,4,5]}
+}, "(stuff:(product_ids:[i]))", check_errors_0);
+
+
+test('Testing objects inside array', {
+	stuff: [{product_id: 1, name: 'abc'}]
+}, "(stuff:[(product_id:i,name:s)])", check_errors_0);
+
+test('Testing nested objects inside array', {
+	products: [{
+		def: {
+			product_id: 1
+		},
+		message: 'abc',
+		def2: {
+			p1: 1,
+			p2: 'abc',
+			def3: {
+				abc: 'abc'
+			}
+		}
+	}]
+}, "(products: [(def: (product_id:i), def2: (p1:i,p2:s,def3:(abc:s)), message:s)])", check_errors_0);
+
+test('Testing nested objects with missing fields inside array', {
+	products: [{
+		def: {
+			product_id: 1
+		},
+		message: 'abc',
+		def2: {
+			p1: 1,
+			p2: 'abc',
+			def3: {
+				abc: 'abc'
+			}
+		}
+	}, {def: {product_id: 2}}]
+}, "(products: [(def: (product_id:i), def2: (p1:i,p2:s,def3:(abc:s)), message:s)])", check_errors_2);
+
+test('Testing array inside nested objects', {
+	product: {
+		def: {
+			product_ids: [1,2,3]
+		}
+	}
+}, "(product: (def: (product_ids:[i])))", check_errors_0);
+
+test('Testing invalid array inside nested objects', {
+	product: {
+		def: {
+			product_ids: [1,'s',3]
+		}
+	}
+}, "(product: (def: (product_ids:[i])))", check_errors_1);
+
+test('Testing array of objects inside nested objects', {
+	product: {
+		def: {
+			products: [
+				{product_id: 1},
+				{product_id: 2},
+				{product_id: 3}
+			]
+		}
+	}
+}, "(product: (def: (products:[(product_id:i)]])))", check_errors_0);
+
+test('Testing missing item in array of objects inside nested objects', {
+	product: {
+		def: {
+			products: [
+				{product_id: 1},
+				{product_isd: 2},
+				{product_id: 3}
+			]
+		}
+	}
+}, "(product: (def: (products:[(product_id:i)]])))", check_errors_1);
+
+
+
+
